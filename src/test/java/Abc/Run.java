57df6ea7b8900fca8f9418c165490f267d7feb38
Saurabh Singh-EXTERNAL 10059665 <Saurabh.Singh1-EXTERNAL@gemalto.com>
2019-05-19 12:00:57 +0530

    sanjeev project

---
+++ src/test/java/Abc/Run.java
@@ -0,0 +1,18 @@
+package Abc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Run {
+
+    @Test
+    public void a()
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\sanjeev.kumar\\Desktop\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+        Assert.assertTrue(false);
+    }
+}
